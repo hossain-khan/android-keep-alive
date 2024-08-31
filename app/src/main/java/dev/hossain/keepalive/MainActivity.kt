0b6d14184912ad15d33a4de495a0b1df81e90d10
Hossain Khan <hello@hossain.dev>
2024-08-31 12:56:59 -0400

    [ADDED] Post notification permission

--- app/src/main/java/dev/hossain/keepalive/MainActivity.kt
+++ app/src/main/java/dev/hossain/keepalive/MainActivity.kt
@@ -1,10 +1,16 @@
 package dev.hossain.keepalive
 
+import android.Manifest.permission.POST_NOTIFICATIONS
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,10 +18,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import dev.hossain.keepalive.service.WatchdogService
 import dev.hossain.keepalive.ui.theme.KeepALiveTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<Array<String>>
+    private val TAG = "MainActivity"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -29,10 +40,34 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        requestPermissionLauncher = this.registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            if (permissions[POST_NOTIFICATIONS] == true) {
+                // Permission granted, you can post notifications
+                Log.d(TAG, "onCreate: Permission granted")
+            } else {
+                // Permission denied, handle accordingly
+                Log.d(TAG, "onCreate: Permission denied")
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissionLauncher.launch(arrayOf(POST_NOTIFICATIONS))
+            }
+        }
     }
 
     override fun onStart() {
         super.onStart()
+        startWatchdogService()
+    }
+
+    private fun startWatchdogService() {
         val serviceIntent = Intent(this, WatchdogService::class.java)
         startService(serviceIntent)
     }
