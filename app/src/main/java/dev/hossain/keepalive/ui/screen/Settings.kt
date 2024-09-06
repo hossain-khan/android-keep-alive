a5d24446ec7dea5a176a844b20a98cec9536229d
Hossain Khan <hello@hossain.dev>
2024-09-05 20:14:58 -0400

    [UPDATE] ktlint format

--- app/src/main/java/dev/hossain/keepalive/ui/screen/Settings.kt
+++ app/src/main/java/dev/hossain/keepalive/ui/screen/Settings.kt
@@ -23,7 +23,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
@@ -52,7 +51,7 @@ fun SettingsScreen(navController: NavHostController) {
         navController = navController,
         viewModel = viewModel,
         context = context,
-        modifier = Modifier.padding(horizontal = 16.dp)
+        modifier = Modifier.padding(horizontal = 16.dp),
     )
 }
 
@@ -84,7 +83,7 @@ fun AppListScreen(
 
         Button(
             onClick = { showDialog.value = true },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
         ) {
             Text("Add App")
         }
