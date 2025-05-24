53062cb0a87ad395812d20b4e7ba5f797b6927ac
Hossain Khan <hello@hossain.dev>
2025-05-24 11:58:48 -0400

    [ADDED] Serialized name - just in case

--- app/src/main/java/dev/hossain/keepalive/data/model/AppActivityLog.kt
+++ app/src/main/java/dev/hossain/keepalive/data/model/AppActivityLog.kt
@@ -1,5 +1,8 @@
 package dev.hossain.keepalive.data.model
 
+import androidx.annotation.Keep
+import dev.hossain.keepalive.service.WatchdogService
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import java.util.Date
 
@@ -8,35 +11,43 @@ import java.util.Date
  *
  * This captures the state of an app when checked by the [WatchdogService].
  */
+@Keep
 @Serializable
 data class AppActivityLog(
     /**
      * Package ID of the application being monitored
      */
+    @SerialName("packageId")
     val packageId: String,
     /**
      * Human-readable name of the application
      */
+    @SerialName("appName")
     val appName: String,
     /**
      * Whether the app was detected as recently running when checked
      */
+    @SerialName("wasRunningRecently")
     val wasRunningRecently: Boolean,
     /**
      * Whether the app was attempted to be started
      */
+    @SerialName("wasAttemptedToStart")
     val wasAttemptedToStart: Boolean,
     /**
      * Unix timestamp (milliseconds) when this check was performed
      */
+    @SerialName("timestamp")
     val timestamp: Long,
     /**
      * Whether force start app setting was enabled at the time of check
      */
+    @SerialName("forceStartEnabled")
     val forceStartEnabled: Boolean,
     /**
      * Optional message with additional details about the operation
      */
+    @SerialName("message")
     val message: String = "",
 ) {
     /**
