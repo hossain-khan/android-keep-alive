03e5095c39928def45bdc2401c2a3e701bd79c5b
Hossain Khan <hello@hossain.dev>
2025-07-04 09:22:16 -0400

    [FIXED] Lint

--- app/src/main/java/dev/hossain/keepalive/ui/screen/AppActivityLogScreen.kt
+++ app/src/main/java/dev/hossain/keepalive/ui/screen/AppActivityLogScreen.kt
@@ -20,8 +20,8 @@ import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -154,7 +154,7 @@ fun AppActivityLogScreen(
 
                     items(logs) { logEntry ->
                         ActivityLogItem(logEntry)
-                        Divider()
+                        HorizontalDivider()
                     }
                 }
             }
