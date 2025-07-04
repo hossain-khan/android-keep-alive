a7549233924fd7fd868f58c92da6e037768beba1
Hossain Khan <hello@hossain.dev>
2025-07-04 19:44:27 -0400

    [FIXED] Compiler issue

--- app/src/main/java/dev/hossain/keepalive/util/NotificationHelper.kt
+++ app/src/main/java/dev/hossain/keepalive/util/NotificationHelper.kt
@@ -73,11 +73,7 @@ class NotificationHelper(private val context: Context) {
 
         val notificationIntent = Intent(context, MainActivity::class.java)
         val pendingIntentFlags =
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-            } else {
-                PendingIntent.FLAG_UPDATE_CURRENT
-            }
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
         val pendingIntent =
             PendingIntent.getActivity(context, 0, notificationIntent, pendingIntentFlags)
 
