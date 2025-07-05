0957bef644293a0290ffd7b8e18e0c27d56b7bfa
Hossain Khan <hello@hossain.dev>
2025-07-04 20:40:15 -0400

    [BUMP] App version for next release
    Also added some recent screenshots

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
          * or you can override View.onWindowVisibilityChanged() to get notified whenever the visibility changes.
          */
         targetSdk = 34
-        versionCode = 15
-        versionName = "2.1"
+        versionCode = 16
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
