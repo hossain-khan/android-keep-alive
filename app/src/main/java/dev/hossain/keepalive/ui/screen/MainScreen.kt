4d701072728d9a9cea7b76339e6e134c807a836c
Hossain Khan <hello@hossain.dev>
2025-12-01 03:58:20 -0500

    fix: remove unused import to pass ktlint check

--- app/src/main/java/dev/hossain/keepalive/ui/screen/MainScreen.kt
+++ app/src/main/java/dev/hossain/keepalive/ui/screen/MainScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
